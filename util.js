b6b774835f58a1406661e0aa4eab2e2679d9ae6b
Yanathan <Yanathan@users.noreply.github.com>
2014-04-21 12:51:12 -0700

    Update util.js

--- util.js
+++ util.js
@@ -22,10 +22,6 @@ function newTurn() {
 
 function newGame(){
     $("#gameInitializer").hide();
-    $("#gameBoard").append('<p id="deckTest">Deck:</p>'
-    '<p id="discardPile">Discard Pile:</p>'
-    '<p id="moneyCountId">Money: <span id="displayMoney"></span></p>'
-    '<p id="actionCountId">Actions: <span id="displayActions"></span></p>'
-    '<p id="buysCountId">Buys: <span id="displayBuys"></span></p>'
+    $("#gameBoard").append('<p id="deckTest">Deck:</p><p id="discardPile">Discard Pile:</p><p id="moneyCountId">Money: <span id="displayMoney"></span></p><p id="actionCountId">Actions: <span id="displayActions"></span></p><p id="buysCountId">Buys: <span id="displayBuys"></span></p>'
     );
 }
