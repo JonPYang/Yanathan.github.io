7d58016e4a8cd4836e5371189fe70cc4a8de25e9
Yanathan <Yanathan@users.noreply.github.com>
2014-04-21 11:06:05 -0700

    Create util.js

---
+++ util.js
@@ -0,0 +1,16 @@
+function newTurn() {
+    while(hand.length > 0) {
+        discard(hand[0]);
+    }
+    if (playerDeck.length >= drawCount) {
+        add(playerDeck);
+    } 
+    else {
+        reDeck(discardPile);
+        add(playerDeck);
+    }
+    removeBuyableCards();
+    buyCount = 1;
+    moneyCount = 0;
+    updateAll();
+}
