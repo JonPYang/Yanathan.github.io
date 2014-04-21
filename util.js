3930fb947d669ce8d534106622e052a37cf2f590
Yanathan <Yanathan@users.noreply.github.com>
2014-04-21 11:11:19 -0700

    Update util.js

--- util.js
+++ util.js
@@ -1,3 +1,4 @@
+<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
 $(document).ready(function(){
   $("p").click(function(){
     $(this).hide();
