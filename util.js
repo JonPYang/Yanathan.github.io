ed25f6dbda40e6ca63c02f2f055d057c9cda01fa
Yanathan <Yanathan@users.noreply.github.com>
2014-04-21 15:50:48 -0700

    Update util.js

--- util.js
+++ util.js
@@ -106,6 +106,8 @@ function buyList(){
     //var buyableCardArray = listOfBuyableCards()
     if(buyCount>=1) {
         for (var i in buyableCards) {
+            var card = cards[i];
+            cardId++;
             $("#buyableCardsList").append("<input id='"+cardId+"'>");
             $("#"+cardId).attr("type","button").attr("value",card.name).attr("onclick","onCardPress("+cardId+")");
             $("#"+cardId).data(cardId.toString(), card);
