f19bbc1d4d5b8b2819d8ea028fc1eefdc741600f
Yanathan <Yanathan@users.noreply.github.com>
2014-04-21 22:07:16 -0700

    Update util.js

--- util.js
+++ util.js
@@ -28,7 +28,7 @@ function newGame(){
 function newTurn() {
     if(hand.length > 0) {
         var handSearch = $('#playableHand input');
-        $.each(handSearch, function(i, value) {
+        $().each(handSearch, function(i, value) {
             discard("fromHand",value.attr('id'))
             
             });
