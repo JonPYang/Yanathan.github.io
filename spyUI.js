8ee09c4ff6c59d5ab4ca6dbc7d86ff0689d9670c
Yanathan <Yanathan@users.noreply.github.com>
2014-08-01 12:49:41 -0700

    Create spyUI.js

---
+++ spyUI.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){        });
+
+$('#Missions').mousedown(function(event) {
+    switch (event.which) {
+        case 1:
+            alert('Left Mouse button pressed.');
+            break;
+        case 2:
+            alert('Middle Mouse button pressed.');
+            break;
+        case 3:
+            alert('Right Mouse button pressed.');
+            break;
+        default:
+            alert('You have a strange Mouse!');
+    }
+});
